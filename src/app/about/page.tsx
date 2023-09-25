b6d14e7f96cac9cac83016f05d52f06243e40f92
Memo Diaz <121352998+memodiaz44@users.noreply.github.com>
2023-09-25 11:25:11 -0600

    fixed types

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -4,7 +4,7 @@ const About = () => {
             <div className="pb-20"></div>
 
             <h1>About</h1>
-        <h2>The Crypto App is a sophisticated web application that seamlessly retrieves real-time data from the Coingecko API, providing users with a comprehensive and interactive experience. Its feature set includes a robust search filtering mechanism and an intuitive pagination system, ensuring users can effortlessly access and navigate the wealth of information available."</h2>
+        <h2>The Crypto App is a sophisticated web application that seamlessly retrieves real-time data from the Coingecko API, providing users with a comprehensive and interactive experience. Its feature set includes a robust search filtering mechanism and an intuitive pagination system, ensuring users can effortlessly access and navigate the wealth of information available.</h2>
         </div>
     )
 }
